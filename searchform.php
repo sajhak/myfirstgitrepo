193bff8008684cb6c52c531042be7158d8912c0c
sajhak@gmail.com <nirmal@nirmal.(none)>
2013-03-19 19:40:45 -0700

    db app- fix 1

--- searchform.php
+++ searchform.php
@@ -3,9 +3,9 @@
 
 <?php
 
-mysql_connect (10.33.16.70:3306, root, dkmdxtif);
+mysql_connect ('10.33.16.70:3306', 'root', 'dkmdxtif');
 
-mysql_select_db (test);
+mysql_select_db ('test');
 
 if ($fname == "")
 {$fname = '%';}
