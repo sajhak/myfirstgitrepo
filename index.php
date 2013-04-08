c4f4fee79d7e1a5608a1356d66f99cb1b4623cd7
nirmal@wso2.com <nirmal@wso2.com>
2013-04-08 05:44:44 +0530

    test5

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Welcome to WSO2 Stratos 2 (Beta2) PaaS !!!!! :-)</p>';
+ <?php echo '<p>Welcome to WSO2 Stratos2 (Beta2) PaaS !!!!! :-)</p>';
  // current time
  echo date('h:i:s') . "\n";
  // sleep for 10 seconds
- sleep(20);
+ #sleep(20);
  //
  // wake up !
  echo date('h:i:s') . "\n";
